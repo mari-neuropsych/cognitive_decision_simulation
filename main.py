6298cd56a10d639364a2abf28545f5c24c3a6ed9
Mariam Muhammad <moa40466@gmail.com>
2025-09-01 12:23:56 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Main file to run the full simulation
+
+import generate_data
+import scatter_plot
+import boxplot
+
+print("Full simulation completed. CSV and plots are saved in 'data/' and 'plots/' folders.")
